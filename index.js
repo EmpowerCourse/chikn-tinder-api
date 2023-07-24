0b87b8bc12730526c52f7975dc36b41ad4675ae3
John Crowley <john@sightsource.net>
2023-07-24 00:51:23 -0400

    ssl config

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ const pool = new Pool({
   database: process.env.DB_NAME || "chikn_tinder",
   password: process.env.DB_PASS || "postgres",
   port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false, // don't verify SSL certificate
+  }
 });
 
 const API_KEY = process.env.API_KEY;
-console.log(API_KEY);
 
 const app = express();
 
